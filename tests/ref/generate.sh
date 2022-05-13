4d1e49133176c1556eaebae20bf03787db868cf8
Anton Kolesov <anthony.kolesov@gmail.com>
2022-05-13 11:33:16 +0300

    Add simple test cases
    Use diff to compare generated output with readelf.

---
+++ tests/ref/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+READELF=readelf
+
+for f in test1 arc
+do
+    file=../${f}.x
+    $READELF -W --file-header $file &> file-header.${f}.txt
+    $READELF -W --program-headers $file &> program-headers.${f}.txt
+    $READELF -W --section-headers $file &> section-headers.${f}.txt
+    # $READELF -W --section-details $file &> section-details.${f}.txt
+    $READELF -W --symbols $file &> symbols.${f}.txt
+    $READELF -W --string-dump=.dynstr --string-dump=.strtab --string-dump=.shstrtab $file &> strings.${f}.txt
+done
---
+++ tests/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PYTHON="C:\\Python310\\python.exe"
+
+for f in test1 arc
+do
+    file=$f.x
+    $PYTHON ../parse_elf.py --file-header $file &> file-header.${f}.txt
+    $PYTHON ../parse_elf.py --program-headers $file &> program-headers.${f}.txt
+    $PYTHON ../parse_elf.py --section-headers $file &> section-headers.${f}.txt
+    # $PYTHON ../parse_elf.py --section-details $file &> section-details.${f}.txt
+    $PYTHON ../parse_elf.py --symbols $file &> symbols.${f}.txt
+    $PYTHON ../parse_elf.py --string-dump=.dynstr --string-dump=.strtab --string-dump=.shstrtab $file &> strings.${f}.txt
+done
+
+for f in test1 arc
+do
+    for t in file-header program-headers section-headers strings symbols
+    do
+        diff --strip-trailing-cr -uN ref/$t.$f.txt $t.$f.txt
+    done
+done
