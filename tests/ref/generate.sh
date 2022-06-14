9a4d47c976489a2ee913074c5dd55111893cc1d6
Anton Kolesov <anthony.kolesov@gmail.com>
2022-06-14 23:56:48 +0300

    Store test data files in separate folders
    One folder per test executable.

--- tests/ref/generate.sh
+++ tests/ref/generate.sh
@@ -3,11 +3,14 @@
 READELF=readelf
 OBJDUMP=objdump
 
-cp -L /usr/bin/gcc test-gcc
+cp -L /usr/bin/gcc ../test-gcc.x
 
 for f in test1 test-x86 test-gcc
 do
-    file=../${f}.x
+    file=$(pwd)/../${f}.x
+    odir=$f
+    mkdir -p $odir
+    cd $odir
     $READELF -W --file-header $file &> file-header.${f}.txt
     $READELF -W --program-headers $file &> program-headers.${f}.txt
     $READELF -W --section-headers $file &> section-headers.${f}.txt
@@ -20,6 +23,26 @@ do
     $READELF -W --string-dump=.dynstr --string-dump=.strtab --string-dump=.shstrtab $file &> strings.${f}.txt
     $OBJDUMP --dwarf $file &> dwarf.${f}.txt
     # Strip lines that I don't want to generate in parse_elf.py
+    $OBJDUMP --dwarf=rawline $file |& tail -n+3 >rawline.${f}.txt
+    # $OBJDUMP --dwarf=decodedline $file |& tail -n+3 > decodedline.${f}.txt
+    # $OBJDUMP --dwarf=info $file |& tail -n+3 > info.${f}.txt
+    # $OBJDUMP --dwarf=abbrev $file |& tail -n+3 > abbrev.${f}.txt
+    # $OBJDUMP --dwarf=pubnames $file |& tail -n+3 > pubnames.${f}.txt
+    # $OBJDUMP --dwarf=aranges $file |& tail -n+3 > aranges.${f}.txt
+    # $OBJDUMP --dwarf=macro $file |& tail -n+3 > macro.${f}.txt
     $OBJDUMP --dwarf=frames $file |& tail -n+3 > dwarf-frames.${f}.txt
     $OBJDUMP --dwarf=frames-interp $file |& tail -n+3 > dwarf-frames-interp.${f}.txt
+    # $OBJDUMP --dwarf=str $file |& tail -n+3 > str.${f}.txt
+    # $OBJDUMP --dwarf=loc $file |& tail -n+3 > loc.${f}.txt
+    # $OBJDUMP --dwarf=Ranges $file |& tail -n+3 > Ranges.${f}.txt
+    # $OBJDUMP --dwarf=pubtypes $file |& tail -n+3 >pubtypes.${f}.txt
+    # $OBJDUMP --dwarf=gdb_index $file |& tail -n+3 > gdb_index.${f}.txt
+    # $OBJDUMP --dwarf=trace_info $file |& tail -n+3 > trace_info.${f}.txt
+    # $OBJDUMP --dwarf=trace_abbrev $file |& tail -n+3 > trace_abbrev.${f}.txt
+    # $OBJDUMP --dwarf=trace_aranges $file |& tail -n+3 > trace_aranges.${f}.txt
+    # $OBJDUMP --dwarf=addr $file |& tail -n+3 > addr.${f}.txt
+    # $OBJDUMP --dwarf=cu_index $file |& tail -n+3 > cu_index.${f}.txt
+    # $OBJDUMP --dwarf=links $file |& tail -n+3 > links.${f}.txt
+    # $OBJDUMP --dwarf=follow-links $file |& tail -n+3 > follow-links.${f}.txt
+    cd - >/dev/null
 done
--- tests/test.sh
+++ tests/test.sh
@@ -4,19 +4,24 @@ PYTHON="C:\\Python310\\python.exe"
 
 for f in test1 test-x86 test-gcc
 do
-    file=$f.x
-    $PYTHON ../parse_elf.py --file-header $file &> file-header.${f}.txt
-    $PYTHON ../parse_elf.py --program-headers $file &> program-headers.${f}.txt
-    $PYTHON ../parse_elf.py --section-headers $file &> section-headers.${f}.txt
-    # $PYTHON ../parse_elf.py --section-details $file &> section-details.${f}.txt
-    $PYTHON ../parse_elf.py --symbols $file &> symbols.${f}.txt
-    $PYTHON ../parse_elf.py --notes $file &> notes.${f}.txt
-    $PYTHON ../parse_elf.py --relocs $file &> relocs.${f}.txt
-    $PYTHON ../parse_elf.py --dynamic $file &> dynamic.${f}.txt
-    $PYTHON ../parse_elf.py --version-info $file &> version-info.${f}.txt
-    $PYTHON ../parse_elf.py --string-dump=.dynstr --string-dump=.strtab --string-dump=.shstrtab $file &> strings.${f}.txt
-    $PYTHON ../parse_elf.py --dwarf=frames $file &> dwarf-frames.${f}.txt
-    $PYTHON ../parse_elf.py --dwarf=frames-interp $file &> dwarf-frames-interp.${f}.txt
+    parse=$(pwd)/../parse_elf.py
+    file=$(pwd)/$f.x
+    odir=$f
+    mkdir -p $odir
+    cd $odir
+    $PYTHON $parse --file-header $file &> file-header.${f}.txt
+    $PYTHON $parse --program-headers $file &> program-headers.${f}.txt
+    $PYTHON $parse --section-headers $file &> section-headers.${f}.txt
+    # $PYTHON $parse --section-details $file &> section-details.${f}.txt
+    $PYTHON $parse --symbols $file &> symbols.${f}.txt
+    $PYTHON $parse --notes $file &> notes.${f}.txt
+    $PYTHON $parse --relocs $file &> relocs.${f}.txt
+    $PYTHON $parse --dynamic $file &> dynamic.${f}.txt
+    $PYTHON $parse --version-info $file &> version-info.${f}.txt
+    $PYTHON $parse --string-dump=.dynstr --string-dump=.strtab --string-dump=.shstrtab $file &> strings.${f}.txt
+    $PYTHON $parse --dwarf=frames $file &> dwarf-frames.${f}.txt
+    $PYTHON $parse --dwarf=frames-interp $file &> dwarf-frames-interp.${f}.txt
+    cd - >/dev/null
 done
 
 for f in test1 test-x86 test-gcc
@@ -24,6 +29,6 @@ do
     # Not testing --notes because GNU_PROPERTY_TYPE_0 is currently not supported.
     for t in file-header program-headers section-headers symbols relocs dynamic version-info strings dwarf-frames dwarf-frames-interp
     do
-        diff --strip-trailing-cr -uN ref/$t.$f.txt $t.$f.txt
+        diff --strip-trailing-cr -uN ref/$f/$t.$f.txt $f/$t.$f.txt
     done
 done
