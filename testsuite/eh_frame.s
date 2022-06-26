b75d72bbb7584ced6be824be6e2e9e5860a033e0
Anton Kolesov <anthony.kolesov@gmail.com>
2022-06-26 19:34:46 +0300

    Start adding a new testsuite
    This testsuite will contain more targeted test-cases to provide better
    coverage. Scripts measure coverage.

---
+++ testsuite/eh_frame.s
@@ -0,0 +1,11 @@
+.globl	_start
+.type	_start, @function
+_start:
+	.cfi_startproc simple
+	.long 0
+	.cfi_def_cfa 0, 16
+	.cfi_rel_offset 1, 8
+	.long 0
+	.cfi_adjust_cfa_offset 16
+	.cfi_rel_offset 2, 8
+	.cfi_endproc
---
+++ testsuite/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PYTHON="C:\\Python310\\python.exe"
+COVERAGE_FILE=coverage.bin
+COVERAGE_RES=coverage-result
+PFLAGS="-mtrace --count --file=$COVERAGE_FILE --coverdir=$COVERAGE_RES"
+parse=$(pwd)/../parse_elf.py
+
+test=eh_frame
+
+rm -rf $COVERAGE_RES $COVERAGE_FILE
+for test in eh_frame
+do
+    $PYTHON $PFLAGS --ignore-dir='C:\Python310\lib' $parse --dwarf=frames ${test}.x > ${test}.gen.txt
+    $PYTHON $PFLAGS --ignore-dir='C:\Python310\lib' $parse --dwarf=frames-interp --dwarf=decodedline ${test}.x >> ${test}.gen.txt
+    diff --strip-trailing-cr -uN ${test}.ref.txt ${test}.gen.txt
+done
+$PYTHON -mtrace --report --file=$COVERAGE_FILE --missing --summary --coverdir=$COVERAGE_RES
