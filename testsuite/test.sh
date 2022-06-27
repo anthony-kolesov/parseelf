87851c17697965cf7c6fb91126b83c655142721f
Anton Kolesov <anthony.kolesov@gmail.com>
2022-06-27 09:04:04 +0300

    Add a simple C program test case in testsuite

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -4,15 +4,16 @@ PYTHON="C:\\Python310\\python.exe"
 COVERAGE_FILE=coverage.bin
 COVERAGE_RES=coverage-result
 PFLAGS="-mtrace --count --file=$COVERAGE_FILE --coverdir=$COVERAGE_RES"
+DW_CATEGORIES="--dwarf=rawline --dwarf=info --dwarf=abbrev --dwarf=aranges --dwarf=frames --dwarf=str"
 parse=$(pwd)/../parse_elf.py
 
 test=eh_frame
 
 rm -rf $COVERAGE_RES $COVERAGE_FILE
-for test in eh_frame
+for test in eh_frame test1
 do
     $PYTHON $PFLAGS --ignore-dir='C:\Python310\lib' $parse -f -l -S -s -r -d -V -n ${test}.x > ${test}.gen.txt
-    $PYTHON $PFLAGS --ignore-dir='C:\Python310\lib' $parse --dwarf=frames ${test}.x >> ${test}.gen.txt
+    $PYTHON $PFLAGS --ignore-dir='C:\Python310\lib' $parse $DW_CATEGORIES ${test}.x >> ${test}.gen.txt
     $PYTHON $PFLAGS --ignore-dir='C:\Python310\lib' $parse --dwarf=frames-interp --dwarf=decodedline ${test}.x >> ${test}.gen.txt
     diff --strip-trailing-cr -uN ${test}.ref.txt ${test}.gen.txt
 done
